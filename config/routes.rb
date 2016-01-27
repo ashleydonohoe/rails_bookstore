9f86bcdef3974cd4a388e7957a5fa80703e3343d
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-01-27 01:10:29 -0500

    added Bootstrap, controlers, and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,69 @@
 Rails.application.routes.draw do
+  get 'publishers/new'
+
+  get 'publishers/create'
+
+  get 'publishers/update'
+
+  get 'publishers/edit'
+
+  get 'publishers/destroy'
+
+  get 'publishers/index'
+
+  get 'publishers/show'
+
+  get 'authors/new'
+
+  get 'authors/create'
+
+  get 'authors/update'
+
+  get 'authors/edit'
+
+  get 'authors/destroy'
+
+  get 'authors/index'
+
+  get 'authors/show'
+
+  get 'categories/new'
+
+  get 'categories/create'
+
+  get 'categories/update'
+
+  get 'categories/edit'
+
+  get 'categories/destroy'
+
+  get 'categories/index'
+
+  get 'categories/show'
+
+  get 'books/new'
+
+  get 'books/create'
+
+  get 'books/update'
+
+  get 'books/edit'
+
+  get 'books/destroy'
+
+  get 'books/index'
+
+  get 'books/show'
+
+  get 'about/index'
+
+  get 'about' => "about#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+ root 'books#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -12,7 +72,10 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resources :books
+  resources :authors
+  resources :publishers
+  resources :categories
 
   # Example resource route with options:
   #   resources :products do
